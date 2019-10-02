00f356380ea34a03132d6871cde0bc8050b81677
Denis Girko <denis.girko@gmail.com>
2019-10-02 10:13:10 +0300

    Added tests

---
+++ db_test.go
@@ -0,0 +1,56 @@
+package db
+
+import (
+	"testing"
+
+	"github.com/shopspring/decimal"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestQprintf(t *testing.T) {
+	var strPtr *string
+	var decimalPtr *decimal.Decimal
+	var cases = []struct {
+		SQL            string
+		params         Params
+		expectedResult string
+	}{
+		// strings
+		{
+			":a, :b",
+			Params{"a": "value_a", "b": "value_b"},
+			"'value_a', 'value_b'",
+		},
+		// nil, nil pointer
+		{
+			":a, :b",
+			Params{"a": nil, "b": strPtr},
+			"NULL, NULL",
+		},
+		// overlapping param names
+		{
+			":a, :a_b, :a b",
+			Params{"a": "value_a", "a_b": "value_b"},
+			"'value_a', 'value_b', 'value_a' b",
+		},
+		// decimal, pointer to decimal
+		{
+			":a, :b",
+			Params{"a": decimal.NewFromFloat(0.0005), "b": decimalPtr},
+			"0.0005, NULL",
+		},
+		// param name collides with a type specification
+		{
+			"'1'::int",
+			Params{"int": 1},
+			"'1'::int",
+		},
+	}
+
+	for _, c := range cases {
+		result, err := qprintf(c.SQL, c.params)
+		assert.NoError(t, err)
+		assert.Equal(t, c.expectedResult, result)
+	}
+}
